4549d5e500251714683a478d4e59d0c5c18bdc0d
Oliver-Goth <Olivergoth00@gmail.com>
2023-11-13 11:28:54 +0100

    doenx2

---
+++ ProgRecordsTests/Repository/MusicRecordsRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgRecords.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgRecords.Repository.Tests
+{
+    [TestClass()]
+    public class MusicRecordsRepositoryTests
+    {
+        [TestMethod()]
+        public void MusicRecordsRepositoryTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
