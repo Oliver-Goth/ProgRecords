fd42386dd34d6d560d93e5f2e85d9982e46c854e
philipv99 <philipv9913@gmail.com>
2023-11-16 12:24:08 +0100

    somthing

--- ProgRecordsTests/Repository/MusicRecordsRepositoryTests.cs
+++ ProgRecordsTests/Repository/MusicRecordsRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgRecords.Class;
 using ProgRecords.Repository;
 using System;
 using System.Collections.Generic;
@@ -14,13 +15,20 @@ namespace ProgRecords.Repository.Tests
         [TestMethod()]
         public void MusicRecordsRepository_Test()
         {
-            throw new NotImplementedException();
+            MusicRecordsRepository repository = new MusicRecordsRepository();
+
+            Assert.IsInstanceOfType(repository, typeof(MusicRecordsRepository));
+            Assert.AreEqual(repository.Records.Count(), 3);
         }
 
         [TestMethod()]
         public void Get_Test()
         {
-            throw new NotImplementedException();
+            MusicRecordsRepository repository = new MusicRecordsRepository();
+            MusicRecord record = new MusicRecord();
+
+            List<MusicRecord> item = repository.Get();
+            Assert.AreEqual(item.Count(), 3);
         }
     }
 }
